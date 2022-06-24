7ac7549bbe4106ed68ba4b5d66c36fbe10776588
m-k-fe <fsd_hack@hotmail.com>
2022-06-24 14:38:55 +0000

    header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      App Component
+      <Header />
+      <main>Content</main>
+      <footer>Footer</footer>
     </div>
   );
 }
